5a995100bd1086b48826b6872d5c585482167c69
MaTtiaa01 <mattia2001@live.it>
2022-11-14 15:21:10 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<template>
+    <header>
+        <img src="" alt="">
+        <h1></h1>
+    </header>
+
+</template>
+
+
+<style>
+
+</style>
