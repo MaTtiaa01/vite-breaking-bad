347591750c71d5c556dc8591b0b4ccd4527f9600
MaTtiaa01 <mattia2001@live.it>
2022-11-14 18:34:44 +0100

    add MainCommands component

--- src/components/MainCommands.vue
+++ src/components/MainCommands.vue
@@ -6,11 +6,25 @@ export default {
 
 <template>
     <div class="main_commands">
+        <form>
 
+            <select>
+                <option value="Breaking-Bad">Breaking-Bad</option>
+                <option value="Rick-and-Morty">Rick-and-Morty</option>
+            </select>
+
+        </form>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
+form {
+    padding: 1rem 0.5rem;
 
+    select {
+        padding: 0.25rem 1rem;
+        border-radius: 5px;
+    }
+}
 </style>
